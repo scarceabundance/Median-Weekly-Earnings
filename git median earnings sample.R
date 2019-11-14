c3dd9207ae26fd48af2c86b1985c8cb71a3d1c99
scarceabundance <scarceabundance@Outlook.com>
2019-11-14 12:55:26 -0800

    Minor Adjustments made to x aesthetic code, and changed color palette.

--- git median earnings sample.R
+++ git median earnings sample.R
@@ -18,10 +18,23 @@ ls.str(BLSHA_PEarnings)
 ##### PERCENT CHANGE IN EARNINGS/WORKERS #####
 ##### Earnings #####
 
-ggplot(BLSHA_PEarnings, aes(fill=Race, y=Percent_Change, x="Period"), alpha("black", 0.5)) + 
+ggplot(BLSHA_PEarnings, aes(fill=Race, y=Percent_Change, x=Period), alpha("black", 0.5)) + 
   geom_col(position="dodge", width = 6, color="black")+
   facet_grid(~ Gender)+ 
   labs(title = "Percent Change in Median Weekly Earnings",
        subtitle = "(2016 - 2017)",
        caption = "Data: U.S. Bureau of Labor Statistics, Current Population Survey (CPS)")
 
+##### Changing Plot Detail #####
+##### Changing color palette #####
+
+g <- ggplot(BLSHA_PEarnings, aes(fill=Race, y=Percent_Change, x=Period), alpha("black", 0.5)) + 
+  geom_col(position="dodge", width = 6, color="black")+
+  facet_grid(~ Gender)+ 
+  labs(title = "Percent Change in Median Weekly Earnings",
+       subtitle = "(2016 - 2017)",
+       caption = "Data: U.S. Bureau of Labor Statistics, Current Population Survey (CPS)")
+
+p <- g + scale_fill_manual(values = wes_palette("Royal1"))
+
+p
